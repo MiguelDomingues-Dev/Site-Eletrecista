c7244f2222f9bcafe93ff0dc578c12aecbc75726
Jiraya660 <85039271+Jiraya660@users.noreply.github.com>
2023-02-08 16:03:45 -0300

    add modo escuro

--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -41,3 +41,35 @@ const mobileNavbar = new MobileNavbar(
   ".nav-list li"
 );
 mobileNavbar.init();
+
+// modo - DARK
+
+const changeThemeBtn = document.querySelector("#change-theme");
+
+// Toggle dark mode
+function toggleDarkmode() {
+  document.body.classList.toggle("dark");
+}
+
+// Load light or dark mode
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark");
+
+    if(darkMode) {
+      toggleDarkmode();
+    }
+}
+
+loadTheme ();
+
+changeThemeBtn.addEventListener("change", function() {
+  toggleDarkmode();
+
+  // Save or remove dark mode
+  localStorage.removeItem("dark");
+
+    if(document.body.classList.contains("dark") ) {
+      localStorage.setItem("dark", 1);
+    }
+});
+
